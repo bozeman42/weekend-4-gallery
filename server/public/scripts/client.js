516eeeb9bfb5f9f059f752abb0fc087ee9aae5e7
Aaron Kvarnlov-Leverty <bozeman42@gmail.com>
2017-10-28 15:46:44 -0500

    Went a little wild. some css done as well.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,5 +2,19 @@ var galleryApp = angular.module('myApp',[]);
 
 galleryApp.controller('GalleryController',function($http){
   var gc = this;
-  gc.helloWorld = "Controller works.";
+  gc.images =
+  [{filename: 'amess.jpg',desc: "A description", likes: 0, mode: true },
+  {filename: 'pileofstuff.jpg',desc: "A description", likes: 0, mode: true },
+  {filename: 'amess.jpg',desc: "A description", likes: 0, mode: true },
+  {filename: 'pileofstuff.jpg',desc: "A description", likes: 0, mode: true },
+  {filename: 'amess.jpg',desc: "A description", likes: 0, mode: true },
+  {filename: 'pileofstuff.jpg',desc: "A description", likes: 0, mode: true }];
+
+  gc.toggleDesc = function(image){
+    image.mode = !image.mode;
+  };
+
+  gc.like = function(image){
+    image.likes += 1;
+  };
 });

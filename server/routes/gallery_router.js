8ea2b5c44d7bd106db03a8544724c53c68ba7572
Aaron Kvarnlov-Leverty <bozeman42@gmail.com>
2017-10-29 14:21:10 -0500

    getting routes ready

--- server/routes/gallery_router.js
+++ server/routes/gallery_router.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var images =
-[{filename: 'amess.jpg',desc: "A description", likes: 0, views: 0, mode: true, comments: [] },
+[{filename: 'dellstour.jpg',desc: "Melissa and I at the Wisconsin Dells. The boat tour of the Lower Dells is fantastic!", likes: 0, views: 0, mode: true, comments: [] },
 {filename: 'pileofstuff.jpg',desc: "A description", likes: 0, views: 0, mode: true, comments: [] },
 {filename: 'amess.jpg',desc: "A description", likes: 0, views: 0, mode: true, comments: [] },
 {filename: 'pileofstuff.jpg',desc: "A description", likes: 0, views: 0, mode: true, comments: [] },
@@ -20,6 +20,10 @@ router.post('/',function(req,res){
   console.log(req.body);
   images.push(req.body);
   res.sendStatus(200);
+});
+
+router.put('/views/:id', function(req,res){
+
 });
 
 // like image

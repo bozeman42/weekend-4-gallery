fe26e1a2bd125eaad1135cdc2015901c57073d38
Aaron Kvarnlov-Leverty <bozeman42@gmail.com>
2017-10-29 11:59:57 -0500

    view counter added

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,14 +3,17 @@ var galleryApp = angular.module('myApp',[]);
 galleryApp.controller('GalleryController',function($http){
   var gc = this;
   gc.images =
-  [{filename: 'amess.jpg',desc: "A description", likes: 0, mode: true },
-  {filename: 'pileofstuff.jpg',desc: "A description", likes: 0, mode: true },
-  {filename: 'amess.jpg',desc: "A description", likes: 0, mode: true },
-  {filename: 'pileofstuff.jpg',desc: "A description", likes: 0, mode: true },
-  {filename: 'amess.jpg',desc: "A description", likes: 0, mode: true },
-  {filename: 'pileofstuff.jpg',desc: "A description", likes: 0, mode: true }];
+  [{filename: 'amess.jpg',desc: "A description", likes: 0, views: 0, mode: true },
+  {filename: 'pileofstuff.jpg',desc: "A description", likes: 0, views: 0, mode: true },
+  {filename: 'amess.jpg',desc: "A description", likes: 0, views: 0, mode: true },
+  {filename: 'pileofstuff.jpg',desc: "A description", likes: 0, views: 0, mode: true },
+  {filename: 'amess.jpg',desc: "A description", likes: 0, views: 0, mode: true },
+  {filename: 'pileofstuff.jpg',desc: "A description", likes: 0, views: 0, mode: true }];
 
   gc.toggleDesc = function(image){
+    if (image.mode === true){
+      image.views += 1;
+    }
     image.mode = !image.mode;
   };
 
